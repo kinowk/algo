90c06bd66d66ba8e87f95486c96b31df4052dab6
kinowk <62692361+kinowk@users.noreply.github.com>
2026-03-24 22:25:56 +0900

    [Bronze III] Title: 친구 친구, Time: 1004 ms, Memory: 201584 KB -BaekjoonHub

---
+++ 백준/Bronze/10865. 친구 친구/친구 친구.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int[] arr = new int[n];
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken()) - 1;
+            int y = Integer.parseInt(st.nextToken()) - 1;
+            arr[x]++;
+            arr[y]++;
+        }
+
+        for (int i : arr) {
+            System.out.println(i);
+        }
+    }
+}

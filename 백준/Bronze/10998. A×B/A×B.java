9716bc87c7c11f5a265594bf31a6a64dae0e5898
kinowk <62692361+kinowk@users.noreply.github.com>
2026-03-29 22:19:27 +0900

    [Bronze V] Title: A×B, Time: 104 ms, Memory: 14200 KB -BaekjoonHub

---
+++ 백준/Bronze/10998. A×B/A×B.java
@@ -0,0 +1,10 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] values = br.readLine().split(" ");
+        int result = Integer.parseInt(values[0]) * Integer.parseInt(values[1]);
+        System.out.println(result);
+    }
+}

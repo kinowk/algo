8d75e3a43dd6871926aac06896472c4a33781cff
kinowk <62692361+kinowk@users.noreply.github.com>
2026-03-29 22:27:15 +0900

    [Bronze V] Title: 사칙연산, Time: 112 ms, Memory: 14176 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.java
@@ -0,0 +1,16 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] values = br.readLine().split(" ");
+        int a = Integer.parseInt(values[0]);
+        int b = Integer.parseInt(values[1]);
+        
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / b);
+        System.out.println(a % b);
+    }
+}
